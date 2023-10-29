67906a27194ea8fb37f8aadf2ded549e433296f2
KHAZRI Omar <omar.khazri@soprahr.com>
2023-10-29 17:07:07 +0100

    Add config

---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+
+
+class Config:
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SECRET_KEY = "dev"
+    # set optional bootswatch theme
+    FLASK_ADMIN_SWATCH = 'Lux'
+    SQLALCHEMY_DATABASE_URI = "sqlite:///bookOnline.db"
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv("TEST_DATABASE_URL")
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
