25b0379a7a6b16ed3e37d1f78957601d0b8ad0ce
KHAZRI Omar <omar.khazri@soprahr.com>
2023-10-29 17:08:24 +0100

    Add main app

---
+++ bookOnline.py
@@ -0,0 +1,9 @@
+from app import db
+from app.routes import app
+from app.models import User
+from app.models import Book
+from app.models import Author
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db,User=User,Book=Book,Author=Author)
