831b16ccd3e5cc06968a5463e033ad6383b48f37
KHAZRI Omar <omar.khazri@soprahr.com>
2023-10-30 11:44:22 +0100

    Add language Model

--- bookOnline.py
+++ bookOnline.py
@@ -3,7 +3,7 @@ from app.routes import app
 from app.models import User
 from app.models import Book
 from app.models import Author
-
+from app.models import Language
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db,User=User,Book=Book,Author=Author)
+    return dict(db=db,User=User,Book=Book,Author=Author,Language=Language)
